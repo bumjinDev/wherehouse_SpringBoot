798de56b46368262ee909b39568a75d1a73f1de6
bumjinDev <bumjin.dev@gmail.com>
2025-01-08 16:15:07 +0900

    test code 작성

---
+++ wherehouse/src/test/java/com/wherehouse/wherehouse/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.wherehouse.wherehouse.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+	@Test
+	void addition() {
+		assertEquals(2, 1 + 1); // 이 테스트는 성공합니다.
+	}
+}
