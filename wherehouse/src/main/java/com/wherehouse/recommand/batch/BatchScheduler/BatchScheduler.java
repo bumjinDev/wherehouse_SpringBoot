9def257a7e89b7fcc21df343e18c0b0edb4cde44
bumjinDev <bumjin.dev@gmail.com>
2026-02-11 00:00:15 +0900

    리뷰시스템페이지네이션개선

--- wherehouse/src/main/java/com/wherehouse/recommand/batch/BatchScheduler/BatchScheduler.java
+++ wherehouse/src/main/java/com/wherehouse/recommand/batch/BatchScheduler/BatchScheduler.java
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
  * * @author 정범진
  * @since 2025-12-05
  */
-//@Component
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class BatchScheduler {
@@ -83,9 +83,9 @@ public class BatchScheduler {
     }
 
     // 매달 새벽 4시 수행
-//    @Scheduled(cron = "0 0 4 * * ?")
+    @Scheduled(cron = "0 0 4 * * ?")
     // 테스트를 위해 즉시 실행 (필요 시 cron으로 변경)
-//    @Scheduled(fixedDelay = Long.MAX_VALUE, initialDelay = 5000)
+    @Scheduled(fixedDelay = Long.MAX_VALUE, initialDelay = 5000)
     public void executeBatchProcess() {
         log.info("=== 부동산 매물 데이터 배치 처리 시작 (Data Collection Phase) ===");
 
